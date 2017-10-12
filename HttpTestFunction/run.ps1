ec828dd34c34576bc6635425472f76bd261bf883
Michael Barton <michael.barton@hotelscombined.com>
2017-10-12 11:14:33 +1100

    remove last two params from r script

--- HttpTestFunction/run.ps1
+++ HttpTestFunction/run.ps1
@@ -4,13 +4,11 @@ $v1Bookings = $requestBody.v1Bookings
 $v1Visitors = $requestBody.v1Visitors
 $v2Bookings = $requestBody.v2Bookings
 $v2Visitors = $requestBody.v2Visitors
-$tStat = $requestBody.tStat
-$pValueMax = $requestBody.pValueMax
 
-if ($v1Bookings -and $v1Visitors -and $v2Bookings -and $v2Visitors -and $tStat -and $pValueMax) 
+if ($v1Bookings -and $v1Visitors -and $v2Bookings -and $v2Visitors) 
 {
     cd D:\home\site\wwwroot\HttpTestFunction
-    $result = d:\home\R-3.3.3\bin\x64\Rscript.exe script.r $v1Bookings $v1Visitors $v2Bookings $v2Visitors $tStat $pValueMax 2>&1    
+    $result = d:\home\R-3.3.3\bin\x64\Rscript.exe script.r $v1Bookings $v1Visitors $v2Bookings $v2Visitors 2>&1    
     Out-File -Encoding Ascii -FilePath $res -inputObject "Hello $result"
 }
 else
